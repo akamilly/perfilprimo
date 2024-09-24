7ca4563a7eb6ff83e09b49488e1e10ec1963c099
akamilly <kamillyvp.veleiros@gmail.com>
2024-09-24 16:37:58 -0300

    update contato roda pe

--- js/script.js
+++ js/script.js
@@ -13,3 +13,19 @@ function changetheme () {
 
     toggletheme.addEventListener("click", changetheme);
     /* fim da função mudar a cor */
+
+
+
+accordionHeaders.forEach(header => {
+    header.addEventListener /* header.addEventListener("click", () => {...}) 
+    Função: adiciona um "ouvinte de eventos" (event listener) ao cabeçario. Nesse caso, ele "ouve" o evento de clique.
+    O que faz: Toda vez que o usuário clicar em um header, a função anônima (dentro dos {...}) será executada. */
+    ("click", () => {
+        const accordionItem = header.parantElement;
+        const accordionActive = accordionItem.classList.contains("active"); /* verificar se esta ativo */
+
+        accordionActive ? accordionItem.classList.remove("active") : accordionItem.classList.add("active");
+    }); /* adicionar ação ao clique  */
+
+    /* Normalmente, o acordeão (accordion) é usado para mostrar/ocultar bocos de informações sem sobrecarregar o usuário com muito conteúdo de uma vez */
+});
